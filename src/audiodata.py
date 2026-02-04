dc55e50d95519ec59e8edb6a8d9ddea55bb7302b
Cadia <nadiafauliii@gmail.com>
2026-02-05 00:08:03 +0100

    wczytanie próbek z path

--- src/audiodata.py
+++ src/audiodata.py
@@ -1,5 +1,9 @@
+import librosa
+import numpy
+
 class AudioData:
     #inicjlizacja
     def __init__(self, audio_file_path):
         self.audio_file_path = audio_file_path
-        self.data_array = None
+        self.data_array, self.samplerate = librosa.load(audio_file_path, sr=None, mono=True)
+        
--- src/mainloop.py
+++ src/mainloop.py
@@ -5,7 +5,9 @@ def mainloop_fun():
     audio_file_path = ask_path_filedialog()
     loaded_data = AudioData(audio_file_path)
     print("ŚCIEŻKA MIMIMIMI:", loaded_data.audio_file_path)
-
+    print("Sample rate:", loaded_data.samplerate)
+    print("Próbki:", loaded_data.data_array)
+    print("Długość:", len(loaded_data.data_array))
     while True:
         print("MEOW")
         print("KOKO")
