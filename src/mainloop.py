e7ff4b492a44c3efb710749f9ba9a3685fdaeb3d
Szymon Duda <91761037+MisterChamster@users.noreply.github.com>
2026-02-07 00:25:08 +0100

    Prints update

--- src/mainloop.py
+++ src/mainloop.py
@@ -4,10 +4,10 @@ from src.audiodata import AudioData
 def mainloop_fun():
     audio_file_path = ask_path_filedialog()
     loaded_data = AudioData(audio_file_path)
-    print("ŚCIEŻKA MIMIMIMI:", loaded_data.audio_file_path)
+    print("ŚCIEŻKA    :", loaded_data.audio_file_path)
     print("Sample rate:", loaded_data.samplerate)
-    print("Próbki:", loaded_data.data_array)
-    print("Długość:", len(loaded_data.data_array))
+    print("Próbki:     ", loaded_data.data_array)
+    print("Długość:    ", len(loaded_data.data_array))
     loaded_data.draw_tone()
     while True:
         print("MEOW")
