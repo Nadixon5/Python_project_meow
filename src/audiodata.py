1c945bb4b7e53b00e008f8419aa791efd00a14df
Szymon Duda <91761037+MisterChamster@users.noreply.github.com>
2026-02-07 00:15:16 +0100

    Methods decl, fields

--- src/audiodata.py
+++ src/audiodata.py
@@ -1,9 +1,32 @@
 import librosa
-import numpy
+import numpy as np
 
 class AudioData:
+    audio_file_path: str
+    data_array: np.ndarray
+    samplerate: int
+
     #inicjlizacja
     def __init__(self, audio_file_path):
         self.audio_file_path = audio_file_path
         self.data_array, self.samplerate = librosa.load(audio_file_path, sr=None, mono=True)
-        
+
+
+    def fast_fourier(self):
+        return
+
+
+    def apply_lowpass(self):
+        return
+
+
+    def apply_hipass(self):
+        return
+
+
+    def shpow_spectogram(self):
+        return
+
+
+    def show_chart(self):
+        return
